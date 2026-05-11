4bfcc477e4bb61647bd9797f1d77c184830b943d
grommasdietz <info@grommas-dietz.com>
2026-05-11 15:32:18 +0200

    feat!: rewrite plugin entry point and panel overlay

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 use Kirby\Cms\App;
 
+$autoload = __DIR__ . '/vendor/autoload.php';
+
+if (is_file($autoload)) {
+    require_once $autoload;
+}
+
 App::plugin('grommasdietz/hidden-characters', []);
